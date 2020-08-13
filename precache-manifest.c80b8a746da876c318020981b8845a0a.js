3214e54a385a47202e97436e10f31715b7c1707d
joshinasla <joshinasla18@gmail.com>
2020-08-13 09:01:17 +0545

    Updates

---
+++ precache-manifest.c80b8a746da876c318020981b8845a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0b06b10e456d91c6bc9b5196599b52",
+    "url": "/react-assignment-3/index.html"
+  },
+  {
+    "revision": "8e378609922902f9ea40",
+    "url": "/react-assignment-3/static/css/main.e7fba854.chunk.css"
+  },
+  {
+    "revision": "865b732ff555d2ccba6b",
+    "url": "/react-assignment-3/static/js/2.8f6c736e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-assignment-3/static/js/2.8f6c736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e378609922902f9ea40",
+    "url": "/react-assignment-3/static/js/main.345896bd.chunk.js"
+  },
+  {
+    "revision": "c647ba50d11303d027d3",
+    "url": "/react-assignment-3/static/js/runtime-main.9f1f39bf.js"
+  }
+]);
